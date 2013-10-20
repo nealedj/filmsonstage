a690d772e4de79596ed80458d6d2772db7246e4b
David Neale <neale.dj@gmail.com>
2013-10-20 17:41:25 +0100

    async-up the middleware to add drinkstatic nodes to context

--- drinkstatic/middleware.py
+++ drinkstatic/middleware.py
@@ -1,4 +1,5 @@
 from google.appengine.api import memcache
+from google.appengine.ext import ndb
 from drinkstatic.models import Node, NodeList, NodeGroup
 from django.conf import settings
 
@@ -6,53 +7,25 @@ __author__ = 'davneale'
 
 class DrinkStaticMiddleware(object):
     def process_request(self, request):
-        self.set_nodes(request)
-        self.set_archived_nodes(request)
-        self.set_live_node_groups(request)
-        self.set_all_node_groups(request)
-        self.set_archived_node_groups(request)
+        context_var_types = {
+            'drinkstatic_nodes': (NodeList, lambda: Node.query().filter(Node.v_archived==False)),
+            'drinkstatic_archived_nodes': (NodeList, lambda: Node.query().filter(Node.v_archived==True)),
+            'drinkstatic_live_node_groups': (lambda m: m, lambda: NodeGroup.query().filter(NodeGroup.group_type==NodeGroup.NodeGroupType.live)),
+            'drinkstatic_all_node_groups': (lambda m: m, lambda: NodeGroup.query().filter(NodeGroup.group_type==NodeGroup.NodeGroupType.all_nodes)),
+            'drinkstatic_archived_node_groups': (lambda m: m, lambda: NodeGroup.query().filter(NodeGroup.group_type==NodeGroup.NodeGroupType.archived))
+        }
 
-    def set_nodes(self, request):
-        key = 'drinkstatic_nodes'
-        nodes = None #memcache.get(key)
-        if not nodes:
-            nodes = NodeList(list(Node.query().filter(Node.v_archived==False)))
-            memcache.set(key, nodes, time=settings.DRINKSTATIC_MEMCACHE_TIME)
+        context_results = memcache.get_multi(context_var_types.keys())
 
-        request.drinkstatic_nodes = nodes
+        futures = {}
+        for k,v in context_var_types.items():
+            if k not in context_results:
+                futures[k] = v[1]().fetch_async()
 
-    def set_archived_nodes(self, request):
-        key = 'drinkstatic_archived_nodes'
-        nodes = None #memcache.get(key)
-        if not nodes:
-            nodes = NodeList(list(Node.query().filter(Node.v_archived==True)))
-            memcache.set(key, nodes, time=settings.DRINKSTATIC_MEMCACHE_TIME)
+        ndb.Future.wait_all(futures.values())
+        for k,v in futures.items():
+            context_results[k] = context_var_types[k][0](list(v.get_result()))
+            memcache.set(k, context_results[k], time=settings.DRINKSTATIC_MEMCACHE_TIME)
 
-        request.drinkstatic_archived_nodes = nodes
-
-    def set_live_node_groups(self, request):
-        key = 'drinkstatic_live_node_groups'
-        node_groups = None #memcache.get(key)
-        if not node_groups:
-            nodes = list(NodeGroup.query().filter(NodeGroup.group_type==NodeGroup.NodeGroupType.live))
-            memcache.set(key, nodes, time=settings.DRINKSTATIC_MEMCACHE_TIME)
-
-        request.drinkstatic_live_node_groups = node_groups
-
-    def set_all_node_groups(self, request):
-        key = 'drinkstatic_all_node_groups'
-        node_groups = None #memcache.get(key)
-        if not node_groups:
-            nodes = list(NodeGroup.query().filter(NodeGroup.group_type==NodeGroup.NodeGroupType.all_nodes))
-            memcache.set(key, nodes, time=settings.DRINKSTATIC_MEMCACHE_TIME)
-
-        request.drinkstatic_all_node_groups = node_groups
-
-    def set_archived_node_groups(self, request):
-        key = 'drinkstatic_archived_node_groups'
-        node_groups = None #memcache.get(key)
-        if not node_groups:
-            nodes = list(NodeGroup.query().filter(NodeGroup.group_type==NodeGroup.NodeGroupType.archived))
-            memcache.set(key, nodes, time=settings.DRINKSTATIC_MEMCACHE_TIME)
-
-        request.drinkstatic_archived_node_groups = node_groups
+        for k,v in context_results.items():
+            setattr(request, k, v)
